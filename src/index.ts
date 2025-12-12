04d85207d6c1a706066da9ff1f9a64f4b4c51a8c
ricobetmarketing <marketing@rico.bet>
2025-12-12 08:12:15 +0000

    Enable browser rendering with puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-import { renderHtml } from "./renderHtml";
+import puppeteer from "@cloudflare/puppeteer";
+import type { ExportedHandler } from "@cloudflare/workers-types";
 
 export default {
-	async fetch(request, env) {
-		const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
-		const { results } = await stmt.all();
+  async fetch(request: Request, env: any) {
+    const browser = await puppeteer.launch(env.BROWSER);
+    const page = await browser.newPage();
 
-		return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-			headers: {
-				"content-type": "text/html",
-			},
-		});
-	},
-} satisfies ExportedHandler<Env>;
+    await page.setViewport({
+      width: 1200,
+      height: 630,
+      deviceScaleFactor: 1,
+    });
+
+    await page.goto("https://rico-quest-cloudflare.pages.dev/", {
+      waitUntil: "networkidle0",
+    });
+
+    const screenshot = await page.screenshot({ type: "png" });
+
+    await browser.close();
+
+    return new Response(screenshot, {
+      headers: {
+        "Content-Type": "image/png",
+      },
+    });
+  },
+} satisfies ExportedHandler;
